5f2a6c63ddd4ddd160f3597af726c4d8c1fee100
devendra-alt <devendramulewa171@gmail.com>
2023-09-29 20:17:52 +0530

    Refactor solver.rb

--- solver.rb
+++ solver.rb
@@ -13,6 +13,8 @@ class Solver
   def fizzbuzz(number)
     if (number % 3).zero? && (number % 5).zero?
       'fizzbuzz'
+    elsif (number % 9).zero?
+      number.to_s
     elsif (number % 3).zero?
       'fizz'
     elsif (number % 5).zero?
