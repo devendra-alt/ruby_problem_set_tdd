1cfd0bd83218d55c2850931d2bae921038036c9a
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-09-29 14:02:27 +0000

    Add Fizz

--- solver.rb
+++ solver.rb
@@ -5,4 +5,17 @@ class Solver
 
     number * factorial(number - 1)
   end
+
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
+    end
+  end
+  
 end
