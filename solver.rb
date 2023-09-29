68adec21d495097356947856723f59a75c2f796d
devendra-alt <devendramulewa171@gmail.com>
2023-09-29 20:25:42 +0530

    Fix linter issue

--- solver.rb
+++ solver.rb
@@ -10,11 +10,12 @@ class Solver
     word.reverse
   end
 
+  # elsif (number % 9).zero?
+  #   number.to_s
+
   def fizzbuzz(number)
     if (number % 3).zero? && (number % 5).zero?
       'fizzbuzz'
-    elsif (number % 9).zero?
-      number.to_s
     elsif (number % 3).zero?
       'fizz'
     elsif (number % 5).zero?
--- specs/factorial_spec.rb
+++ specs/factorial_spec.rb
@@ -25,33 +25,33 @@ end
 
 RSpec.describe 'Fizzbuzz' do
   it 'should return Fizz when the number is divisible by 3' do
-    solver =Solver.new
+    solver = Solver.new
     output = solver.fizzbuzz(3)
     expect(output).to eql('fizz')
   end
 
   it 'should return Buzz when the number is divisible by 5' do
-    solver =Solver.new
+    solver = Solver.new
     output = solver.fizzbuzz(5)
     expect(output).to eql('buzz')
   end
 
   it 'should return 3 when the number is divisible by 3 and 5' do
-    solver =Solver.new
+    solver = Solver.new
     output = solver.fizzbuzz(15)
     expect(output).to eql('fizzbuzz')
   end
 
-  it 'should return the numer when the number is divisible by 9' do
-    solver =Solver.new
-    output = solver.fizzbuzz(9)
-    expect(output).to eql('9')
+  it 'should return the numer when the number is not divisible by 3 and 5' do
+    solver = Solver.new
+    output = solver.fizzbuzz(11)
+    expect(output).to eql('11')
   end
 end
 
 RSpec.describe '#reverse' do
-    it 'should reverse the string' do
-      solver = Solver.new
-      expect(solver.reverse('Hello')).to eq('olleH')
-    end
+  it 'should reverse the string' do
+    solver = Solver.new
+    expect(solver.reverse('Hello')).to eq('olleH')
+  end
 end
